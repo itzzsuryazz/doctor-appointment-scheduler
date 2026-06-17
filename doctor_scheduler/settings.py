4df486d9a07bd73d47eaecbdd4ee9b2778549f14
itzzsuryazz <kingsurya8838@gmail.com>
2026-06-17 19:52:30 +0530

    Add static folder for Railway

--- doctor_scheduler/settings.py
+++ doctor_scheduler/settings.py
@@ -74,8 +74,10 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = []
+if (BASE_DIR / 'static').exists():
+    STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
